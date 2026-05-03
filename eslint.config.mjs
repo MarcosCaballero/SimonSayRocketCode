48908bbf59d0ee7395f4982c69d86930c4648860
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2026-05-03 21:41:42 +0200

    mod eslint rules and add changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,17 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+    { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, 
+        rules: {
+            "no-console": "warn",
+            "prefer-const": "error",
+            "no-var": "error",
+            "semi": ["error", "always"],
+            "quotes": ["error", "double"],
+            "indent": ["error", 4],
+            "linebreak-style": ["error", "unix"],
+        },
+        extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
 ]);
--- index.js
+++ index.js
@@ -4,32 +4,32 @@
 */
 
 const opciones = {
-	top_left: {
-		id: "top_left"
-	},
-	top_right: {
-		id: "top_right"
-	},
-	bottom_left: {
-		id: "bottom_left"
-	},
-	bottom_right: {
-		id: "bottom_right"
-	},
+    top_left: {
+        id: "top_left"
+    },
+    top_right: {
+        id: "top_right"
+    },
+    bottom_left: {
+        id: "bottom_left"
+    },
+    bottom_right: {
+        id: "bottom_right"
+    },
 };
 
 const estadoJuego = {
-	intervalos: {
-		inicio: 1000,
-		paso: 200
-	},
-	segundosInicio: 3,
-	interaciones: false,
-	secuenciaJuego: [],
-	secuenciaUsuario: [],
-	nivelJuego: 0,
-	nivelUsuario: 0,
-	opciones,
+    intervalos: {
+        inicio: 1000,
+        paso: 200
+    },
+    segundosInicio: 3,
+    interaciones: false,
+    secuenciaJuego: [],
+    secuenciaUsuario: [],
+    nivelJuego: 0,
+    nivelUsuario: 0,
+    opciones,
 };
 
 /**
@@ -43,191 +43,193 @@ const botonesDelJuego = document.querySelectorAll(".simon-button");
 */
 
 const activarElemento = (elementoDOM) => {
-	elementoDOM.classList.add('active');
+    elementoDOM.classList.add("active");
 };
 
 const mostrarElemento = (elementoDOM) => {	
-	elementoDOM.classList.add('show');
-	elementoDOM.classList.remove('hide');
+    elementoDOM.classList.add("show");
+    elementoDOM.classList.remove("hide");
 };
 
 const desactivarElemento = (elementoDOM) => {
-	elementoDOM.classList.remove('active')
-}
+    elementoDOM.classList.remove("active");
+};
 
 const ocultarElemento = (elementoDOM) => {
-	elementoDOM.classList.add('hide');
-	elementoDOM.classList.remove('show');
+    elementoDOM.classList.add("hide");
+    elementoDOM.classList.remove("show");
 };
 
 const activarElementos = (elementos) => {
-	elementos.forEach(activarElemento);
+    elementos.forEach(activarElemento);
 };
 
 const desactivarElementos = (elementos) => {
-	elementos.forEach(desactivarElemento);
+    elementos.forEach(desactivarElemento);
 };
 
 // iteracciones
 
 const activarInteracciones = () => {
-	const elementoApp = globalThis.document.getElementById('app');
-	elementoApp.classList.remove('app-background-dark');
+    const elementoApp = globalThis.document.getElementById("app");
+    elementoApp.classList.remove("app-background-dark");
 
-	const elementoTexto = globalThis.document.getElementById('turno_texto');
-	mostrarElemento(elementoTexto);
-	elementoTexto.textContent = 'tu turno';
+    const elementoTexto = globalThis.document.getElementById("turno_texto");
+    mostrarElemento(elementoTexto);
+    elementoTexto.textContent = "tu turno";
 
-	estadoJuego.interaciones = true; 
+    estadoJuego.interaciones = true; 
 };
 
 const desactivarInteracciones = () => {
-	const elementoApp = globalThis.document.getElementById('app');
-	elementoApp.classList.add('app-background-dark');
+    const elementoApp = globalThis.document.getElementById("app");
+    elementoApp.classList.add("app-background-dark");
 
-	const elementoTexto = globalThis.document.getElementById('turno_texto');
-	ocultarElemento(elementoTexto);
+    const elementoTexto = globalThis.document.getElementById("turno_texto");
+    ocultarElemento(elementoTexto);
 
-	estadoJuego.interaciones = false; 
+    estadoJuego.interaciones = false; 
 };
 
 const accionModalInicio = () => {
-	const inputDom = globalThis.document.getElementById('nombre_jugador');
-	const nombreJugador = inputDom.value;
+    const inputDom = globalThis.document.getElementById("nombre_jugador");
+    const nombreJugador = inputDom.value;
 
-	const permitirAcceso = nombreJugador.length;
+    const permitirAcceso = nombreJugador.length;
 
-	if (permitirAcceso) {
-		const elementoModalInicio = globalThis.document.getElementById('inicio_juego');
-		ocultarElemento(elementoModalInicio); 
+    if (permitirAcceso) {
+        const elementoModalInicio = globalThis.document.getElementById("inicio_juego");
+        ocultarElemento(elementoModalInicio); 
 
-		const elementoNombre = globalThis.document.getElementById('nombre_usuario');
-		elementoNombre.textContent = nombreJugador;
-		globalThis.localStorage.setItem('nombre', nombreJugador);
+        const elementoNombre = globalThis.document.getElementById("nombre_usuario");
+        elementoNombre.textContent = nombreJugador;
+        globalThis.localStorage.setItem("nombre", nombreJugador);
 
-		inicializacion();
-	};
+        inicializacion();
+    };
 };
 
 function accionModalFin () {
-	const elementoModalFinal = globalThis.document.getElementById('fin_juego');
-	ocultarElemento(elementoModalFinal);
+    const elementoModalFinal = globalThis.document.getElementById("fin_juego");
+    ocultarElemento(elementoModalFinal);
 
-	inicializacion();
+    inicializacion();
 };
 
 const clickBoton = (id) => {
 
-	if (!estadoJuego.interaciones) {
-		return;
-	};
+    if (!estadoJuego.interaciones) {
+        return;
+    };
 
-	estadoJuego.secuenciaUsuario.push(id);
+    estadoJuego.secuenciaUsuario.push(id);
 
-	const secuenciaJuegoEstaEtapa = estadoJuego.secuenciaJuego[estadoJuego.nivelUsuario];
-	const secuenciaUsuarioEstaEtapa = estadoJuego.secuenciaUsuario[estadoJuego.nivelUsuario];
+    const secuenciaJuegoEstaEtapa = estadoJuego.secuenciaJuego[estadoJuego.nivelUsuario];
+    const secuenciaUsuarioEstaEtapa = estadoJuego.secuenciaUsuario[estadoJuego.nivelUsuario];
 
-	if (secuenciaJuegoEstaEtapa === secuenciaUsuarioEstaEtapa) { //Se validan resultados y se pasa al siguiente nivel
-		estadoJuego.nivelUsuario = estadoJuego.nivelUsuario + 1;	
-	} else { //Aca el juego termina
-		const elementoJuego = globalThis.document.getElementById('juego');
-		ocultarElemento(elementoJuego);
+    if (secuenciaJuegoEstaEtapa === secuenciaUsuarioEstaEtapa) { //Se validan resultados y se pasa al siguiente nivel
+        estadoJuego.nivelUsuario = estadoJuego.nivelUsuario + 1;	
+    } else { //Aca el juego termina
+        const elementoJuego = globalThis.document.getElementById("juego");
+        ocultarElemento(elementoJuego);
 	
 
-		desactivarInteracciones();
+        desactivarInteracciones();
 
-		const elementoModalFinal = globalThis.document.getElementById('fin_juego');
-		mostrarElemento(elementoModalFinal);
+        const elementoModalFinal = globalThis.document.getElementById("fin_juego");
+        mostrarElemento(elementoModalFinal);
 
-		const puntajeDom = globalThis.document.getElementById('puntaje');
-		const puntaje = estadoJuego.nivelJuego 
-		const puntajeFinal = puntaje - 1
-		puntajeDom.textContent = ("Tu puntaje es: " + puntajeFinal);
+        const puntajeDom = globalThis.document.getElementById("puntaje");
+        const puntaje = estadoJuego.nivelJuego; 
+        const puntajeFinal = puntaje - 1;
+        puntajeDom.textContent = ("Tu puntaje es: " + puntajeFinal);
 
-		return ;
+        return ;
 		
-	};
+    };
 	
-	if (estadoJuego.nivelJuego === estadoJuego.nivelUsuario) {
-		desactivarInteracciones();
+    if (estadoJuego.nivelJuego === estadoJuego.nivelUsuario) {
+        desactivarInteracciones();
 
-		estadoJuego.secuenciaUsuario = [];
+        estadoJuego.secuenciaUsuario = [];
 
-		estadoJuego.nivelUsuario = 0;
+        estadoJuego.nivelUsuario = 0;
 	
-		reproducirSecuencia();	
+        reproducirSecuencia();	
 
-	};
+    };
 };
 
 
 const obtenerElementoAleatorio = () => {
-	const opcionesIds = Object.keys(estadoJuego.opciones);
-	const idAleatorio = opcionesIds[Math.floor(Math.random() * opcionesIds.length)];
+    const opcionesIds = Object.keys(estadoJuego.opciones);
+    const idAleatorio = opcionesIds[Math.floor(Math.random() * opcionesIds.length)];
 
-	return estadoJuego.opciones[idAleatorio];
+    return estadoJuego.opciones[idAleatorio];
 };
 
 const reproducirSecuencia = () => {
-	let paso = 0;
+    let paso = 0;
 
-	estadoJuego.secuenciaJuego.push(obtenerElementoAleatorio().id);
-	estadoJuego.nivelJuego = estadoJuego.nivelJuego + 1;
+    estadoJuego.secuenciaJuego.push(obtenerElementoAleatorio().id);
+    estadoJuego.nivelJuego = estadoJuego.nivelJuego + 1;
 
-	const intervalo = setInterval(() => {
-		const pausaPaso = paso % 2 === 1;
+    const intervalo = setInterval(() => {
+        const pausaPaso = paso % 2 === 1;
 
-		const finReproduccion = paso === (estadoJuego.secuenciaJuego.length * 2);
+        const finReproduccion = paso === (estadoJuego.secuenciaJuego.length * 2);
 
-				if (pausaPaso) {
-					desactivarElementos(botonesDelJuego);
-					paso++;
-					return;
-				}
+        if (pausaPaso) {
+            desactivarElementos(botonesDelJuego);
+            paso++;
+            return;
+        }
 
-				if (finReproduccion) {
-					clearInterval(intervalo);
-					desactivarElementos(botonesDelJuego);
-					activarInteracciones();
-					return;
-				};
+        if (finReproduccion) {
+            clearInterval(intervalo);
+            desactivarElementos(botonesDelJuego);
+            activarInteracciones();
+            return;
+        };
 
-		const id = estadoJuego.secuenciaJuego[paso / 2];
-		const referenciaDOM = globalThis.document.getElementById(id);
-		activarElemento(referenciaDOM);	
+        const id = estadoJuego.secuenciaJuego[paso / 2];
+        const referenciaDOM = globalThis.document.getElementById(id);
+        activarElemento(referenciaDOM);	
 		
-		paso++;
-	}, estadoJuego.intervalos.paso);
+        paso++;
+    }, estadoJuego.intervalos.paso);
 };
 
 const inicializacion = () => {
-	let segundosInicio = estadoJuego.segundosInicio;
+    let segundosInicio = estadoJuego.segundosInicio;
 
-	estadoJuego.secuenciaJuego = [];
-	estadoJuego.secuenciaUsuario = [];
-	estadoJuego.nivelJuego = 0;
-	estadoJuego.nivelUsuario = 0;
+    estadoJuego.secuenciaJuego = [];
+    estadoJuego.secuenciaUsuario = [];
+    estadoJuego.nivelJuego = 0;
+    estadoJuego.nivelUsuario = 0;
 
-	const elementoJuego = globalThis.document.getElementById('juego');
-	mostrarElemento(elementoJuego);
+    const elementoJuego = globalThis.document.getElementById("juego");
+    mostrarElemento(elementoJuego);
 
-	const elementoCuentaRegresiva = document.getElementById('cuenta_regresiva');
-	mostrarElemento(elementoCuentaRegresiva);
-	elementoCuentaRegresiva.textContent = segundosInicio;
+    const elementoCuentaRegresiva = document.getElementById("cuenta_regresiva");
+    mostrarElemento(elementoCuentaRegresiva);
+    elementoCuentaRegresiva.textContent = segundosInicio;
 
-	const intervalo = setInterval (() => {
-		segundosInicio--;
+    const intervalo = setInterval (() => {
+        segundosInicio--;
 
-		elementoCuentaRegresiva.textContent = segundosInicio;
+        elementoCuentaRegresiva.textContent = segundosInicio;
 
-		if (segundosInicio === 0) {
-			ocultarElemento(elementoCuentaRegresiva);
-			reproducirSecuencia();
-			clearInterval(intervalo);
-		}
-	}, estadoJuego.intervalos.inicio);
-};
+        if (segundosInicio === 0) {
+            ocultarElemento(elementoCuentaRegresiva);
+            reproducirSecuencia();
+            clearInterval(intervalo);
+        }
+    }, estadoJuego.intervalos.inicio);
+};		
 
-const nombreJugadorStorage = globalThis.localStorage.getItem('nombre');
-const nombre = document.getElementById('nombre_jugador');
-nombre.value = nombreJugadorStorage || '';
+const nombreJugadorStorage = globalThis.localStorage.getItem("nombre");
+const nombre = document.getElementById("nombre_jugador");
+nombre.value = nombreJugadorStorage || "";
+
+const x = 10;
